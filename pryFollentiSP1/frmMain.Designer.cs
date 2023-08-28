11ff950c7acfdbb2424c630138a2abd3073d486d
MatiasFollenti <matiasfollenti@gmail.com>
2023-08-28 16:46:19 -0300

    28/08/2023Agrego Botones

--- pryFollentiSP1/frmMain.Designer.cs
+++ pryFollentiSP1/frmMain.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.btnCrear = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCrear
             // 
-            this.btnCrear.Location = new System.Drawing.Point(70, 345);
+            this.btnCrear.Location = new System.Drawing.Point(12, 345);
             this.btnCrear.Name = "btnCrear";
             this.btnCrear.Size = new System.Drawing.Size(159, 56);
             this.btnCrear.TabIndex = 0;
@@ -41,11 +43,31 @@
             this.btnCrear.UseVisualStyleBackColor = true;
             this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
             // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(197, 345);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(159, 56);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "LEER ARCHIVO";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(382, 345);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(159, 56);
+            this.btnModificar.TabIndex = 2;
+            this.btnModificar.Text = "MODIFICAR ARCHIVO";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(578, 450);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnLeer);
             this.Controls.Add(this.btnCrear);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -57,6 +79,8 @@
         #endregion
 
         private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnModificar;
     }
 }
 
