c103761d36fc78adc23fef7ea0495265a32fd5aa
MatiasFollenti <matiasfollenti@gmail.com>
2023-08-28 16:52:28 -0300

    Probando GitHub

--- pryFollentiSP1/frmMain.Designer.cs
+++ pryFollentiSP1/frmMain.Designer.cs
@@ -35,9 +35,10 @@
             // 
             // btnCrear
             // 
-            this.btnCrear.Location = new System.Drawing.Point(12, 345);
+            this.btnCrear.Location = new System.Drawing.Point(18, 505);
+            this.btnCrear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCrear.Name = "btnCrear";
-            this.btnCrear.Size = new System.Drawing.Size(159, 56);
+            this.btnCrear.Size = new System.Drawing.Size(238, 82);
             this.btnCrear.TabIndex = 0;
             this.btnCrear.Text = "CREAR ARCHIVO";
             this.btnCrear.UseVisualStyleBackColor = true;
@@ -45,30 +46,36 @@
             // 
             // btnLeer
             // 
-            this.btnLeer.Location = new System.Drawing.Point(197, 345);
+            this.btnLeer.Location = new System.Drawing.Point(295, 505);
+            this.btnLeer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLeer.Name = "btnLeer";
-            this.btnLeer.Size = new System.Drawing.Size(159, 56);
+            this.btnLeer.Size = new System.Drawing.Size(238, 82);
             this.btnLeer.TabIndex = 1;
             this.btnLeer.Text = "LEER ARCHIVO";
             this.btnLeer.UseVisualStyleBackColor = true;
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(382, 345);
+            this.btnModificar.Location = new System.Drawing.Point(573, 505);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(159, 56);
+            this.btnModificar.Size = new System.Drawing.Size(238, 82);
             this.btnModificar.TabIndex = 2;
             this.btnModificar.Text = "MODIFICAR ARCHIVO";
             this.btnModificar.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 450);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(849, 658);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnLeer);
             this.Controls.Add(this.btnCrear);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SP1- manejo archivos";
